79d4bc77542fbe6bd15a76e12998a7f15a303484
toma_note <toma0923@outlook.jp>
2023-07-09 21:21:05 +0900

    add programs to check if input is right

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 import random
+import japanize_kivy
 from kivy.app import App, Widget
 from kivy.core.window import Window
 from kivy.properties import StringProperty
 
 
-class Keyboard(Widget):
+class MainLayout(Widget):
     """
     メインレイアウト
     キーボードからの入力を受け取る
@@ -13,10 +14,13 @@ class Keyboard(Widget):
     input_text = StringProperty("")
 
     def __init__(self, **kwargs):
-        super(Keyboard, self).__init__(**kwargs)
+        super(MainLayout, self).__init__(**kwargs)
         self._keyboard = Window.request_keyboard(
             self._keyboard_closed, self, 'text')
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
+
+        self.quiz = Quiz()
+        self.switch_shown_text()
 
     def _keyboard_closed(self):
         print('My keyboard have been closed!')
@@ -26,14 +30,32 @@ class Keyboard(Widget):
     def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
         if keycode[0] == 8:
             # バックスペース
-            self.input_text = self.input_text[:-1]
-        elif len(self.input_text) < 30:
-            self.input_text += text + " "
+            self.input_text = self.input_text[:-2]
 
-        # print(f"The key {keycode} has been pressed")
-        # print(f" - text is {text}")
-        # print(' - modifiers are %r' % modifiers)
-        # print(self.input_text)
+        elif keycode[0] == 13:
+            truly_input_text = self.input_text.replace(" ", "")
+            # Enterが押されたとき、入力が正しければ正解処理に進む
+            if self.quiz.check_input(truly_input_text):
+                self.switch_shown_text()
+            self.input_text = ""
+
+        elif text != None:
+            # 文字数制限以下なら文字を追加
+            if len(self.input_text) < self.len_current_problem * 2:
+                self.input_text += text + " "
+
+        print(f"The key {keycode} has been pressed")
+        print(f" - text is {text}")
+        print(' - modifiers are %r' % modifiers)
+
+    def switch_shown_text(self):
+        """
+        表示されている問題文を次に切り替える
+        """
+        self.quiz.next_problem()
+        self.len_current_problem = len(self.quiz.current[0])  # 問題の英単語の文字数
+        self.ids.question.text = self.quiz.current[1]  # 問題文(日本語)
+        self.ids.under_line.text = "_ " * self.len_current_problem  # アンダーバー
 
 
 class Quiz:
@@ -49,6 +71,8 @@ class Quiz:
         for p in self.raw_problems:
             x = p.split(",")
             self.problems.append(x)
+        
+        self.random_pick_problems(10)
 
     def random_pick_problems(self, required: int):
         """
@@ -57,12 +81,25 @@ class Quiz:
         self.random_problems = random.sample(self.problems, required)
         print(self.random_problems)
 
+    def next_problem(self):
+        """
+        問題を切り替える
+        """
+        self.current = self.random_problems.pop(0)
+
+    def check_input(self, input):
+        """
+        入力が正解かどうか判定する
+        """
+        if input == self.current[0]:
+            return True
+        else:
+            return False
+
 
 class TypingApp(App):
     def build(self):
-        quiz = Quiz()
-        quiz.random_pick_problems(10)
-        return Keyboard()
+        return MainLayout()
 
 
 if __name__ == "__main__":
--- src/test.py
+++ src/test.py
@@ -1,4 +1,2 @@
-a = [1, 2]
-b = [2, 3]
-b.append(a)
-print(b)
+a = "abcd"
+print(a[:-1])
