38ad419207188897405966cef0f93df6d147e53f
toma_note <toma0923@outlook.jp>
2023-07-14 10:05:18 +0900

    add programs to write underline to fill the empty character slot

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ class MainLayout(Widget):
     キーボードからの入力を受け取る
     """
 
-    input_text = StringProperty("")
+    shown_text = StringProperty("")
 
     def __init__(self, **kwargs):
         super(MainLayout, self).__init__(**kwargs)
@@ -20,7 +20,8 @@ class MainLayout(Widget):
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
 
         self.quiz = Quiz()
-        self.switch_shown_text()
+        self.input_text = ""
+        self.switch_shown_problem()
 
     def _keyboard_closed(self):
         print('My keyboard have been closed!')
@@ -30,32 +31,42 @@ class MainLayout(Widget):
     def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
         if keycode[0] == 8:
             # バックスペース
-            self.input_text = self.input_text[:-2]
+            self.input_text = self.input_text[:-1]
+            self.change_shown_text()
 
         elif keycode[0] == 13:
-            truly_input_text = self.input_text.replace(" ", "")
             # Enterが押されたとき、入力が正しければ正解処理に進む
-            if self.quiz.check_input(truly_input_text):
-                self.switch_shown_text()
-            self.input_text = ""
+            if self.quiz.check_input(self.shown_text):
+                self.switch_shown_problem()
+            self.switch_shown_problem() # 現在不正解でも次の問題へ
 
         elif text != None:
             # 文字数制限以下なら文字を追加
-            if len(self.input_text) < self.len_current_problem * 2:
-                self.input_text += text + " "
+            if len(self.input_text) < self.len_current_problem:
+                self.change_shown_text(add_text=text)
 
         print(f"The key {keycode} has been pressed")
         print(f" - text is {text}")
         print(' - modifiers are %r' % modifiers)
 
-    def switch_shown_text(self):
+    def switch_shown_problem(self):
         """
         表示されている問題文を次に切り替える
         """
         self.quiz.next_problem()
-        self.len_current_problem = len(self.quiz.current[0])  # 問題の英単語の文字数
-        self.ids.question.text = self.quiz.current[1]  # 問題文(日本語)
-        self.ids.under_line.text = "_ " * self.len_current_problem  # アンダーバー
+        # 問題の英単語の文字数
+        self.len_current_problem = len(self.quiz.current[0]) 
+        # 問題文(日本語)
+        self.ids.question.text = self.quiz.current[1]
+        # アンダーバーと入力された文字を初期化
+        self.underline = "_" * self.len_current_problem
+        self.input_text = ""
+
+        self.change_shown_text()
+
+    def change_shown_text(self, add_text=""):
+        self.input_text += add_text
+        self.shown_text = " ".join(self.input_text + self.underline[len(self.input_text):])
 
 
 class Quiz:
--- src/test.py
+++ src/test.py
@@ -1,2 +1,71 @@
-a = "abcd"
-print(a[:-1])
+import numpy as np
+
+# 重りを入力(単位はmm)
+M = np.array([0, 199.975, 199.972, 199.973, 199.985, 199.975, 199.99, 199.98])*10**-3
+
+# 幅（3つ）を入力(単位はmm)
+w_av = np.array([16.0, 15.9, 16.0])*10**-3
+
+# 高さ（3つ）を入力(単位はmm)
+t_av = np.array([4.97, 4.9, 5.03])*10**-3
+
+# 傾きを入力
+k = 0.06138995988
+
+
+# dを入力(単位はcm)
+d = 3.25 * 10**-2
+
+# Lを入力(単位はcm)
+L = 40.0 * 10**-2
+
+# 重力加速度
+g = 9.78
+
+# Dを入力(単位はm)
+D_1 = 1.15
+D_2 = 1.15
+
+
+dY_f_u = np.array([30 ,28.7, 27.6, 26.5, 25.1, 23.8, 22.6, 21.5]) * 10 ** -2  # 上り
+dY_f_d = np.array([30 ,28.6, 27.5, 26.2, 25.1, 23.8, 22.6, 21.5]) * 10 ** -2  # 下り
+dY_b_u = np.array([30 ,28.5, 27.2, 26.0, 24.7, 23.5, 22.2, 21.0]) * 10 ** -2  # 上り
+dY_b_d = np.array([29.7 ,28.4, 27.2, 26.0, 24.6, 23.5, 22.2, 21.0]) * 10 ** -2  # 下り
+
+# ここからは入力しなくてよい
+
+w = (w_av[0] + w_av[1] + w_av[2]) / 3
+
+t = (t_av[0] + t_av[1] + t_av[2]) / 3
+
+D = (D_1 + D_2) / 2
+
+dY_A = np.zeros(7)  # dY_A の結果を格納する配列を初期化
+dY = np.zeros(7)  # dY の結果を格納する配列を初期化
+for i in range(0, 7):
+    if i == 0:
+        dY_A[i] = (abs(dY_f_u[i] - dY_f_u[i + 1]) + abs(dY_f_d[i] - dY_f_d[i + 1])) + (
+                abs(dY_b_u[i] - dY_b_u[i + 1]) + abs(dY_b_d[i] - dY_b_d[i + 1]))
+        dY[i] = dY_A[i] / 4
+        print(f'dY[i+1] = {dY[i]}')
+
+    else:
+        dY_A[i] = (abs(dY_f_u[i] - dY_f_u[i + 1]) + abs(dY_f_d[i] - dY_f_d[i + 1])) + (
+                abs(dY_b_u[i] - dY_b_u[i + 1]) + abs(dY_b_d[i] - dY_b_d[i + 1]))
+        dY[i] = dY[i - 1] + (dY_A[i] / 4)
+        print(f'dY[{0}] = {dY[i]}')
+
+e = (d / 2*D) * dY
+print(f'e = {e}[m]')
+
+M_A = np.zeros(8)
+for i in range(1, 8):
+    if i == 1:
+        M_A[i] = M[0] + M[i]
+    else:
+        M_A[i] = M_A[i-1] + M[i]
+    
+    print(f'M[{i}] = {M_A[i]}[kg]')
+
+E = (L**3 * D * g) / ((2 * t**3) * w * d * k)
+print(f'E = {E}[Pa]')
